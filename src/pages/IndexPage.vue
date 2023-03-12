db31b68d09a26fd807f9ab395c1665d9a3757496
luskasouza <zerolucas11@gmail.com>
2023-03-12 17:55:08 -0300

    :white_check_mark: app-pokedrock[10%]

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,49 +1,56 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page
+    ><div class="rows">
+      <q-table
+        dense
+        color="s"
+        :loading="loading"
+        title="Treats"
+        :rows="posts"
+        :columns="columns"
+        row-key="name"
+        class="col"
+      />
+    </div>
+    <!--<div>{{ posts }}</div>-->
   </q-page>
 </template>
 
-<script lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-import { defineComponent, ref } from 'vue';
-
-export default defineComponent({
+<script>
+export default {
   name: 'IndexPage',
-  components: { ExampleComponent },
-  setup() {
-    const todos = ref<Todo[]>([
-      {
-        id: 1,
-        content: 'ct1'
-      },
-      {
-        id: 2,
-        content: 'ct2'
-      },
-      {
-        id: 3,
-        content: 'ct3'
-      },
-      {
-        id: 4,
-        content: 'ct4'
-      },
-      {
-        id: 5,
-        content: 'ct5'
-      }
-    ]);
-    const meta = ref<Meta>({
-      totalCount: 1200
-    });
-    return { todos, meta };
-  }
-});
+  data() {
+    return {
+      loading: false,
+      nextPage: 2,
+      user: {},
+      columns: [
+        { name: 'name', label: 'name', field: 'name', align: 'left' },
+        { name: 'url', label: 'url', field: 'url', align: 'left' },
+      ],
+      posts: [],
+    };
+  },
+  created() {
+    this.user = 'OK';
+  },
+  mounted() {
+    console.log('mounted');
+    return this.getPosts();
+  },
+  methods: {
+    getPosts() {
+      this.$axios
+        .get('https://pokeapi.co/api/v2/pokemon/')
+        .then((response) => {
+          return (this.posts = response.data.results);
+        })
+        .finally(() => {
+          setTimeout(() => {
+            this.loading = false;
+          }, 500);
+        });
+    },
+  },
+};
 </script>
